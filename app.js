6fdc41e8f3036ae90ff2672e71c51c171017cc9c
kindacoder <code.ashutosh@gmail.com>
2018-01-20 19:28:32 +0530

    Deploying to heroku

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express=require('express');
-const socket=require('socket.io');
-const app=express();
-var port=process.env.PORT || 3000;
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+var port = process.env.PORT || 3000;
 
 //listen to port
-const server=app.listen(port);
+const server = app.listen(port);
 
-const io=socket(server);
+const io = socket(server);
 
 ///serve static files:-
 app.use(express.static('public'));
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 
-io.on('connection',function(socket){
-socket.on('chat message',function(data){
-  console.log(data);
-  ///recieved message from user now Spread the message;
-  io.sockets.emit('new',data);
-})
-socket.on('typing',function(userName){
-  io.sockets.emit('typing',userName);
+io.on('connection', function(socket) {
+    socket.on('chat message', function(data) {
+        console.log(data);
+        ///recieved message from user now Spread the message;
+        io.sockets.emit('new', data);
+    })
+    socket.on('typing', function(userName) {
+        io.sockets.emit('typing', userName);
 
-})
+    })
 })
